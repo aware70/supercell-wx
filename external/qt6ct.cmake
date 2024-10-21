96c6baa6c90bdd965903206d3fcd9786cb422a93
AdenKoperczak <awkoperczak@gmail.com>
2024-10-21 16:20:56 -0400

    Add install location for qt6ct for windows (maybe)

--- external/qt6ct.cmake
+++ external/qt6ct.cmake
@@ -4,3 +4,8 @@ set(PROJECT_NAME scwx-qt6ct)
 add_subdirectory(qt6ct/src/qt6ct-common)
 set_target_properties(qt6ct-common PROPERTIES PUBLIC_HEADER qt6ct/src/qt6ct-common/qt6ct.h)
 target_include_directories( qt6ct-common INTERFACE qt6ct/src )
+install(TARGETS qt6ct-common
+        PUBLIC_HEADER DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/qt6ct
+        ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
+        LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+        RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR})
