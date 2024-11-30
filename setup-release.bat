c74a56206c2cf45a1fe9ab4022776c14ca8f6a7a
Dan Paulat <dpaulat@gmail.com>
2024-11-29 21:06:53 -0600

    Add conan profiles to setup scripts

--- setup-release.bat
+++ setup-release.bat
@@ -2,13 +2,18 @@ call tools\setup-common.bat
 
 set build_dir=build-release
 set build_type=Release
+set conan_profile=scwx-win64_msvc2022
 set qt_version=6.8.0
 set qt_arch=msvc2022_64
+
+conan install tools/conan/profiles/%conan_profile% -tf profiles
 
 mkdir %build_dir%
 cmake -B %build_dir% -S . ^
     -DCMAKE_BUILD_TYPE=%build_type% ^
     -DCMAKE_CONFIGURATION_TYPES=%build_type% ^
     -DCMAKE_PREFIX_PATH=C:/Qt/%qt_version%/%qt_arch% ^
-    -DCMAKE_PROJECT_TOP_LEVEL_INCLUDES=external/cmake-conan/conan_provider.cmake
+    -DCMAKE_PROJECT_TOP_LEVEL_INCLUDES=external/cmake-conan/conan_provider.cmake ^
+	-DCONAN_HOST_PROFILE=${conan_profile} ^
+	-DCONAN_BUILD_PROFILE=${conan_profile}
 pause
--- setup-release.sh
+++ setup-release.sh
@@ -3,9 +3,12 @@
 
 build_dir=${1:-build-release}
 build_type=Release
+conan_profile=${2:-scwx-linux_gcc-11}
 qt_version=6.8.0
 qt_arch=gcc_64
 script_dir="$(dirname "$(readlink -f "$0")")"
+
+conan install tools/conan/profiles/${conan_profile} -tf profiles
 
 mkdir -p ${build_dir}
 cmake -B ${build_dir} -S . \
@@ -14,4 +17,6 @@ cmake -B ${build_dir} -S . \
 	-DCMAKE_INSTALL_PREFIX=${build_dir}/${build_type}/supercell-wx \
 	-DCMAKE_PREFIX_PATH=/opt/Qt/${qt_version}/${qt_arch} \
 	-DCMAKE_PROJECT_TOP_LEVEL_INCLUDES=${script_dir}/external/cmake-conan/conan_provider.cmake \
+	-DCONAN_HOST_PROFILE=${conan_profile} \
+	-DCONAN_BUILD_PROFILE=${conan_profile} \
 	-G Ninja
